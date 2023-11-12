2506110bb4a6b5fe0a3e16f10a32f3cbe3acacd2
LRuego <LRuego20@gmail.com>
2023-11-12 18:50:46 +0800

    Enabling Assessment 4 added

---
+++ EA4/Dicee Challenge/index.js
@@ -0,0 +1,20 @@
+var x = Math.floor(Math.random() * 6) + 1;
+var y = Math.floor(Math.random() * 6) + 1;
+
+var img1 = "images/dice" + x + ".png";
+var img2 = "images/dice" + y + ".png";
+
+$('img').eq(0).attr("src", img1);
+$('img').eq(1).attr("src", img2);
+
+if (x > y) {
+    $('h1').text('Player 1 Wins🚩')
+}
+else if (x < y) {
+    $('h1').text('Player 2 Wins🚩')
+}
+else {
+    $('h1').text('Draw!')
+}
+
+$('p').eq(2).text(Math.floor((Math.random() * 5)+1))
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Accomplished Assessments in this Repository:<br>
 Enabling Assessment 1: HTML Personal Site   (old-site.html)<br>
 Enabling Assessment 2: CSS Personal Site    (index.html)<br>
 Enabling Assessment 3: My Brand             (brand.html)<br>
+Enabling Assessment 4: Javascript/JQuery    (EA4\Dicee Challenge\dicee.html & EA4\Drum Kit\drumkit.html)<br>
